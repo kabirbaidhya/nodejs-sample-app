401ee35dbfb3098c3a7210c1a27825edf5bb925f
Kabir <kabirbaidhya@gmail.com>
2016-11-10 00:22:58 +0545

    Hello World

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import Promise from 'bluebird';
+
+global.Promise = Promise;
+
+let app = express();
+let router = express.Router();
+
+// Hello World
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
