02761716a4f471f8c6844c6c22d44fcbd45df8b9
Kabir <kabirbaidhya@gmail.com>
2016-11-18 02:06:09 +0545

    Camelize responses

--- src/server/services/todo.js
+++ src/server/services/todo.js
@@ -1,24 +1,15 @@
-
+import camelize from 'camelize';
 import Todo from '../models/Todo';
 import logger from '../foundation/logger';
 
 export function fetchAll() {
     logger.debug('Fetching all the todos');
 
-    return Todo.fetchAll();
+    return Todo.fetchAll().then(result => result.map(camelizeData));
 }
 
 export function fetch(id) {
-    let item = Todo.where('id', id);
-
-    return item.fetch().then(data => {
-        logger.debug(`Got data for todo item ${id}`, data);
-
-        return (data) ? data : Promise.reject({
-            message:'Todo not found.',
-            status: 404
-        });
-    });
+    return fetchModel(id).then(camelizeData);
 }
 
 export function create(data) {
@@ -29,18 +20,35 @@ export function create(data) {
     return item.save().then(createdItem => {
         logger.debug('Item created', createdItem);
 
-        return fetch(createdItem.id); 
+        return fetch(createdItem.id);
     });
 }
 
 export function update(id, data) {
-    return fetch(id).then(
-        item => item.set(data).save()
-    );
+    return fetchModel(id)
+        .then(item => item.set(data).save())
+        .then(item => fetch(id));
 }
 
 export function destroy(id) {
-    return fetch(id).then(item => {
+    return fetchModel(id).then(item => {
         item.destroy();
     });
+}
+
+function fetchModel(id) {
+    let item = Todo.where('id', id);
+
+    return item.fetch().then(data => {
+        logger.debug(`Got data for todo item ${id}`, data);
+
+        return (data) ? data : Promise.reject({
+            message: 'Todo not found.',
+            status: 404
+        });
+    });
+}
+
+function camelizeData(data) {
+    return camelize(data.attributes);
 }
