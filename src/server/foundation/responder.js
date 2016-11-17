43c2db32484458ed8ea09bf5b1d9a2e06a2c217c
Kabir <kabirbaidhya@gmail.com>
2016-11-18 01:37:48 +0545

    Handle & log errors

--- src/server/foundation/responder.js
+++ src/server/foundation/responder.js
@@ -1,8 +1,14 @@
+import logger from './logger';
+
+const GENERAL_ERROR_MSG = 'An error occurred';
 
 export function respond(req, res, result) {
     result
         .then(data => res.json(data))
-        .catch(err => res.status(err.status || 500).json({
-            error: err
-        }));
+        .catch(err => {
+            const message = err.message || 'An error occurred';
+
+            logger.error(message, err);
+            res.status(err.status || 500).json({error: message});
+        });
 }
