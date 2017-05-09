774dae6e6c52d488cc11abc2eba0df8ba0394c96
vsujan <sujanvaidya@lftechnology.com>
2017-05-09 16:52:28 +0545

    Add knexfile

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+        client: process.env.DB_CLIENT,
+        connection: {
+            database: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
+        }
+    }
+};
